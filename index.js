cf1b8542ded2f472e77f91a1978e5cd487df5f9c
gavayec13 <gavayec13@gmail.com>
2021-03-15 15:06:53 +0200

    fixed select

--- index.js
+++ index.js
@@ -30,13 +30,69 @@ const clearInput = () => {
     inputTitle.value = '';
     inputContent.value = '';
 }
+//////////////////////////////////////////////////////////////////
+const data = [
+    {id: 1, value: 'date'},
+    {id: 2, value: 'name'},
+    {id: 3, value: 'priority'}
+];
+
+const items = data.map(item => {
+    return `
+        <li class="select-item" data-type='item' data-value='${item.id}'>${item.value}</li>
+    `
+})
+
+const createInputSort = () => {
+    const dropContainer = document.createElement('div');
+    const header = document.querySelector('header')
+    dropContainer.classList.add('drop-container');
+    dropContainer.insertAdjacentHTML('afterbegin', `
+        <div id="selectInput" class="select-input">
+            <span>Sort by</span>
+            <i id="arrow" class="fas fa-chevron-down"></i>
+        </div>
+    `)
+    header.append(dropContainer);
+}
+createInputSort();
+
+const createSelect = () => {
+    const selectDrop = document.createElement('div');
+    selectDrop.classList.add('select-dropdown');
+    const dropContainer = document.querySelector('.drop-container');
+    selectDrop.insertAdjacentHTML('afterbegin', `
+        <ul class="select-list">
+           ${items.join('')}
+        </ul>
+    `)
+    dropContainer.append(selectDrop);
+}
 
 const selectToggle = () => {
     const dropDown = document.querySelector('.select-dropdown');
     dropDown.classList.contains('open-select') ? dropDown.classList.remove('open-select') : dropDown.classList.add('open-select');
 }
 
-selectInput.onclick = () => selectToggle();
+const arrowToggle = () => {
+    const arrow = document.getElementById('arrow');
+    if( arrow.classList.contains('fa-chevron-down')) {
+        arrow.classList.remove('fa-chevron-down');
+        arrow.classList.add('fa-chevron-up');
+    } else {
+        arrow.classList.remove('fa-chevron-up');
+        arrow.classList.add('fa-chevron-down');
+    }
+}
+
+selectInput.onclick = (event) => {
+    const {type} = event.target.dataset.value;
+    console.log(type);
+    createSelect();
+    selectToggle();
+    arrowToggle();
+}
+
 
 // 1 drop   date, title.name, priority
 // 2 drop incrising decrising
---
+++ script.js
@@ -0,0 +1,145 @@
+
+const $ = {};
+const container = document.createElement('div');
+let priority = 'green';
+let timeNow;
+function time() {
+    timeNow = new Date().toString().slice(0, 21)
+};
+
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+const renderNotes = (htmlElement, title, content, priority, timeNow, index) => {
+    htmlElement.classList.add('container');
+    htmlElement.insertAdjacentHTML('beforeend', `
+        <div class="my-notes priority-${priority} wrapper-${index}">
+            <button onclick='deleteNote(${index})' class='note-close'>x</button>
+            <p class="note-title">${title}</p>
+            <div class="note-content">${content} ${index}</div>
+            <div class="note-time"><span>${timeNow}</span></div>
+        </div>
+        `)
+    document.body.append(htmlElement);
+}
+
+const notesMap = () => notes.map((note, index) => renderNotes(container, note.title, note.content, note.priority, note.time, index));
+notesMap();
+
+function createModal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <span class="modal-title">New Note</span>
+                    <span id='modalClose' class="modal-close">X</span>
+                </div>
+                <div class="modal-body">
+                    <input id='inputTitle' class='input-title' placeholder='Title'/>
+                    <textarea id='inputContent' class='input-content' placeholder='note...'></textarea>
+                    <div class="form">
+                        <div class="form-radio-btn">
+                            <input id="radio-1" type="radio" name="radio" value="red">
+                            <label for="radio-1">High priority</label>
+                        </div>
+                        <div class="form-radio-btn">
+                            <input id="radio-2" type="radio" name="radio" value="yellow">
+                            <label for="radio-2">Middle priority</label>
+                        </div>
+                        <div class="form-radio-btn">
+                            <input id="radio-3" type="radio" name="radio" value="green">
+                            <label for="radio-3">Low priority</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button id='saveBtn'>Save Note</button>
+                    <button id='closeBtn'>Close</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.append(modal);
+    return modal;
+}
+
+
+$.modal = function(options) {
+    const $modal = createModal(options);
+    const animSpeed = 500;
+    
+    return {
+        open() {
+            $modal.classList.add('open')
+        },
+        close() {
+            $modal.classList.remove('open');
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.remove('hide')
+            }, animSpeed)
+        }
+    }
+}
+
+const newModal = $.modal();
+
+const saveNote = () => {
+    time();
+    if(inputTitle.value && inputContent.value) {
+        notes.push({
+            title: inputTitle.value,
+            content: inputContent.value,
+            priority,
+            time: timeNow,
+        })
+        localStorage.setItem('notes', JSON.stringify(notes));
+
+        let note = notes[notes.length-1];
+        renderNotes(container, note.title, note.content, note.priority, timeNow, notes.length - 1);
+    }
+}
+
+function deleteNote(index) {
+    let el = document.querySelector(`.wrapper-${index}`);
+
+    console.log(notes);
+
+    el.parentElement.remove();
+
+    notes.splice(index, 1);
+    
+    console.log(notes);
+    notesMap();
+    localStorage.setItem('notes', JSON.stringify(notes));
+    
+}
+
+addBtn.onclick = () => newModal.open();
+
+closeBtn.onclick = () => newModal.close();
+
+modalClose.onclick = () => newModal.close();
+
+saveBtn.onclick = () => {
+    radioCheck();
+    saveNote();
+    newModal.close();
+    clearInput();
+};
+
+function radioCheck() {
+    const rad = document.getElementsByName('radio');
+    for(el of rad) {
+        if(el.checked) {
+            priority = el.value;
+        }
+    }
+}
+
+const clearInput = () => {
+    inputTitle.value = '';
+    inputContent.value = '';
+}
