1786e555160b94ecdfb07373fc4aafc8c99250a4
gavayec13 <gavayec13@gmail.com>
2021-03-15 23:43:02 +0200

    fixed select

---
+++ select.js
@@ -0,0 +1,55 @@
+const header = document.querySelector('header');
+const dropContainer = document.createElement('div');
+const dropDown = document.createElement('div');
+
+const data = [
+    {id: '1', value: 'Date'},
+    {id: '2', value: 'Name'},
+    {id: '3', value: 'Priority'}
+];
+
+const createInputSort = () => {
+    dropContainer.classList.add('drop-container');
+    dropContainer.insertAdjacentHTML('afterbegin', `
+        <div id="selectInput" class="select-input">
+            <span id='inputText'>Sort by</span>
+            <i id="arrow" class="fas fa-chevron-down"></i>
+        </div>
+    `);
+    header.append(dropContainer);
+}
+createInputSort();
+
+const items = data.map(item => {
+    return `
+        <li onclick='onclickItems(event)' class="select-item" data-type='item' data-value='${item.value}'>${item.value}</li>
+    `
+});
+
+const createSelect = () => {
+    dropDown.classList.add('select-dropdown');
+    dropDown.insertAdjacentHTML('afterbegin', `
+        <ul class="select-list">
+           ${items.join('')}
+        </ul>
+    `);
+    dropContainer.append(dropDown);
+}
+createSelect();
+
+const selectToggle = () => {
+    dropDown.classList.toggle('open-select');
+    const arrow = document.getElementById('arrow');
+    arrow.classList.toggle('fa-chevron-up');
+}
+
+selectInput.onclick = () => selectToggle();
+
+const onclickItems = (e) => {
+    const inputText = document.getElementById('inputText');
+    inputText.innerHTML = e.target.dataset.value;
+    selectToggle();
+}
+
+// 1 drop   date, title.name, priority
+// 2 drop incrising decrising
